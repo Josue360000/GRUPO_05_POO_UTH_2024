7164ab8a9d28b4962e22924f37d66ac529749fee
Hector Nuñez <hectorjosue360000@gmail.com>
2024-11-07 22:15:28 -0600

    Add files via upload

---
+++ TAREA_GRUPO_05_PERSONA_POO.java
@@ -0,0 +1,10 @@
+package com.mycompany.tarea_grupo_05_persona_poo;
+
+// Clase principal para probar el codigo
+public class TAREA_GRUPO_05_PERSONA_POO {
+public static void main(String[] args) {
+  System.out.println("Codigo en blanco para llenar el programa");       
+}
+}
+
+
