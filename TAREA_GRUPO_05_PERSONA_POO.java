cdbdb95d98590df6f29ca7c7688eaf553db1c6ca
Hector Nuñez <hectorjosue360000@gmail.com>
2024-11-07 22:17:18 -0600

    V1 TAREA_GRUPO_05_PERSONA_POO.java
    En esta actualización se acaba de crear la superclase Persona

--- TAREA_GRUPO_05_PERSONA_POO.java
+++ TAREA_GRUPO_05_PERSONA_POO.java
@@ -1,5 +1,57 @@
 package com.mycompany.tarea_grupo_05_persona_poo;
 
+// Clase abstracta Persona
+abstract class Persona {
+private String nombre;
+private int edad;
+private double sueldo;
+private char sexo;
+
+// Constructor de Persona
+public Persona(String nombre, int edad, double sueldo, char sexo) {
+this.nombre = nombre;
+this.edad = edad;
+this.sueldo = sueldo;
+this.sexo = sexo;
+}
+
+// Metodos getter y setter
+public String getNombre() {
+return nombre;
+}
+
+public void setNombre(String nombre) {
+this.nombre = nombre;
+}
+
+public int getEdad() {
+return edad;
+}
+
+public void setEdad(int edad) {
+this.edad = edad;
+}
+
+public double getSueldo() {
+return sueldo;
+}
+
+public void setSueldo(double sueldo) {
+this.sueldo = sueldo;
+}
+
+public char getSexo() {
+return sexo;
+}
+
+public void setSexo(char sexo) {
+this.sexo = sexo;
+}
+
+// Metodo abstracto que se implementara en las subclases
+public abstract void mostrarInfo();
+}
+
 // Clase principal para probar el codigo
 public class TAREA_GRUPO_05_PERSONA_POO {
 public static void main(String[] args) {
